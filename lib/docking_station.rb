dc5708f94312e04532b535a037a0e5f742da798f
danieljonesdmj <danieljonesdmj@gmail.com>
2018-06-13 17:33:37 +0100

    add exception to code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,23 @@
 require_relative 'bike'
 
 class DockingStation
+
+  def initialize
+    @counter = 0
+  end
+
   def release_bike
+    fail "no bikes available" if @counter < 1
+    @counter -= 1
     Bike.new
   end
+
+  def dock(bike)
+    @counter += 1
+    @bike = bike
+  end
+
+
+# attr_writer :counter
+  attr_reader :bike
 end
