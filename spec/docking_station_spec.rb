23be55616ee1bb7ec3b622ee6627d9650f12e70a
danieljonesdmj <danieljonesdmj@gmail.com>
2018-06-15 13:28:19 +0100

    add raise error tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,24 +3,37 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
+    # subject.dock(bike)
+    # bike = subject.release_bike
     expect(bike).to be_working
   end
 
-  describe '#release_bike' do
-  it "will not release bike if counter is zero" do
-    expect {subject.release_bike}.to raise_error "no bikes available"
-  end
-  end
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
 
   it "docks something" do
     bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+    end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
-    expect(subject).to respond_to(:bike)
+    end
+
+    describe '#release_bike' do
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+
+      end
+      it 'returns error if no bikes available' do
+        expect(subject.release_bike).to raise_error
+      end
+    end
   end
-end
