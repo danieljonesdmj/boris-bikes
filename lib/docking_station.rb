632c0773485339da565b9b7ac9bd9c857fc73515
danieljonesdmj <danieljonesdmj@gmail.com>
2018-06-15 13:26:34 +0100

    added guardian condition

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,22 +2,15 @@ require_relative 'bike'
 
 class DockingStation
 
-  def initialize
-    @counter = 0
-  end
-
   def release_bike
-    fail "no bikes available" if @counter < 1
-    @counter -= 1
-    Bike.new
+    @bike
+    fail "no bikes available" if @bike == nil
   end
 
   def dock(bike)
-    @counter += 1
     @bike = bike
   end
 
-
-# attr_writer :counter
   attr_reader :bike
+
 end
